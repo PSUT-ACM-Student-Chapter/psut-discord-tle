51863ef81279e7aec60f63e343e86ee286bbb8f6
MrMoon <44137590+MrMoon@users.noreply.github.com>
2026-03-04 01:37:55 +0300

    fix font downloader

--- tle/util/font_downloader.py
+++ tle/util/font_downloader.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import urllib.request
+import zipfile
 
 from zipfile import ZipFile
 from io import BytesIO
@@ -11,14 +12,16 @@ URL_BASE = 'https://noto-website-2.storage.googleapis.com/pkgs/'
 FONTS = [constants.NOTO_SANS_CJK_BOLD_FONT_PATH,
          constants.NOTO_SANS_CJK_REGULAR_FONT_PATH]
 
+ARABIC_FONT_URL = 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansArabic-unhinted.zip'
+
 logger = logging.getLogger(__name__)
 
 
 def _unzip(font, archive):
-    with ZipFile(archive) as zipfile:
-        if font not in zipfile.namelist():
+    with ZipFile(archive) as zipfile_obj:
+        if font not in zipfile_obj.namelist():
             raise KeyError(f'Expected font file {font} not present in downloaded zip archive.')
-        zipfile.extract(font, constants.FONTS_DIR)
+        zipfile_obj.extract(font, constants.FONTS_DIR)
 
 
 def _download(font_path):
@@ -29,27 +32,16 @@ def _download(font_path):
 
 
 def maybe_download():
+    os.makedirs(constants.FONTS_DIR, exist_ok=True)
+
     for font_path in FONTS:
         if not os.path.isfile(font_path):
             _download(font_path)
 
-    # Make sure the font directory exists
-    os.makedirs(constants.FONTS_DIR, exist_ok=True)
-
-    # 1. Download CJK Font (Existing behavior)
-    cjk_font_path = os.path.join(constants.FONTS_DIR, 'NotoSansCJKjp-Regular.otf')
-    if not os.path.isfile(cjk_font_path):
-        print('Downloading CJK font...')
-        urllib.request.urlretrieve(CJK_FONT_URL, 'cjk_font.zip')
-        with zipfile.ZipFile('cjk_font.zip', 'r') as zip_ref:
-            zip_ref.extractall(constants.FONT_DIR)
-        os.remove('cjk_font.zip')
-
-    # 2. Download Arabic Font (New behavior)
     arabic_font_path = os.path.join(constants.FONTS_DIR, 'NotoSansArabic-Regular.ttf')
     if not os.path.isfile(arabic_font_path):
-        print('Downloading Arabic font...')
+        logger.info('Downloading Arabic font...')
         urllib.request.urlretrieve(ARABIC_FONT_URL, 'arabic_font.zip')
-        with zipfile.ZipFile('arabic_font.zip', 'r') as zip_ref:
+        with ZipFile('arabic_font.zip', 'r') as zip_ref:
             zip_ref.extractall(constants.FONTS_DIR)
         os.remove('arabic_font.zip')
