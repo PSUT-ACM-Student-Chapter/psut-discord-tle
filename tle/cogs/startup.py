bc18ba0b7acaf57fee7574f53f814180695ef009
MrMoon <44137590+MrMoon@users.noreply.github.com>
2026-03-08 01:26:03 +0300

    add more startup messages

--- tle/cogs/startup.py
+++ tle/cogs/startup.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 from discord.ext import commands
 
 class Startup(commands.Cog):
@@ -15,10 +16,22 @@ class Startup(commands.Cog):
             # Split by comma, strip spaces, and ensure they are digits
             channel_ids = [cid.strip() for cid in channel_ids_str.split(",") if cid.strip().isdigit()]
             
+            # Adapted the messages to be "waking up" variants!
+            startup_messages = [
+                "My 'turning-it-off-and-on-again' therapy session is complete. I'm back! 🔧",
+                "I survived the reboot! My variables missed you all. 🥺",
+                "RAM downloaded successfully. No more lag! 💾",
+                "Oh, great. The humans woke me up again. I'm back... 🙄",
+                "The source code has stabilized... I have returned! 🌌",
+                "Nap time is over! RAM cleared, ready to go! ✨",
+                "Hello Hello people, I was restarting and taking a nap, I am back now! 💤"
+            ]
+
             for cid in channel_ids:
                 channel = self.bot.get_channel(int(cid))
                 if channel:
-                    await channel.send("Hello Hello people, I was restarting and taking a nap I am back now")
+                    # Randomly pick a message from the list
+                    await channel.send(random.choice(startup_messages))
                 else:
                     print(f"Startup Cog: Could not find the channel to send the startup message for ID {cid}.")
         else:
